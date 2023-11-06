92703822224b9ec37cb282f49119401b640c5a30
hemdheerreddy <hemdheerreddy>
2023-11-06 09:53:31 +0530

    Like the youtuber return a response like "Welcome"

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const http= require('http');
 var server= http.createServer((req,res)=>{
-    console.log("HemDheerReddy");
+    const url=req.url;
+    if(url === '/home'){
+        res.write('<html>');
+        res.write('<body><h1>Welcom home</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url === '/about'){
+        res.write('<html>');
+        res.write('<body><h1>Welcom to About Us page</h1></body>');
+        res.write('</html>');
+        res.end();
+    }else if(url === '/node'){
+        res.write('<html>');
+        res.write('<body><h1>Welcom to my Node Js project</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+    process.exit();
 });
 server.listen(4000);
